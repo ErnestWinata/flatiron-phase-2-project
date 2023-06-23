b9f738636b4d8ac7082b0ef49a469f0525633cca
Ernest <ernestwinata@gmail.com>
2023-06-23 09:17:05 -0700

    fix <p> tag above the page

--- landmark-app/src/SelectedDestinations.js
+++ landmark-app/src/SelectedDestinations.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SelectedDestinations = ({ selectedDestinations, destinations }) => {
   return (
     <div>
-      <h2>Your Selections:</h2>
+      <h2>Your travel wish list:</h2>
       <ul>
         {selectedDestinations.map((id) => (
           <li key={id}>{destinations.find((d) => d.id === id).name}</li>
