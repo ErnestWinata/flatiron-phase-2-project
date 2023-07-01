0d3d6074489d769ef88fdcb903c088d62e9efc38
Ernest <ernestwinata@gmail.com>
2023-07-01 06:52:14 -0700

    fix NavBar

--- landmark-app/src/App.js
+++ landmark-app/src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Home';
 import DestinationList from './DestinationList';
+import NavBar from './NavBar';
 
 function App() {
-  return <DestinationList />;
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/destinations" element={<DestinationList />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
+
+
+
--- landmark-app/src/DestinationList.js
+++ landmark-app/src/DestinationList.js
@@ -63,14 +63,6 @@ const DestinationList = () => {
 
   return (
     <div>
-    <h1>Travel Wish List</h1>
-    <p className="description">
-        The world is yours to explore. Which cities have been on your bucket list to visit?
-        Here are some major destinations around the world that are sure to inspire any avid
-        (or occasional) traveler. Check the boxes of the cities that you wish to visit;
-        uncheck if you change your mind. If your dream destination is not on the list,
-        simply add it in the box down below and hit the 'Add Destination' button.
-      </p>
       <form onSubmit={handleSubmit}>
         <div className="grid">
           {destinations.map((destination) => (
